79cf19af764e92d20b529808a7594cdec3ebd051
zhaowei.zhang01 <zhaowei.zhang01@hand-china.com>
2019-02-18 13:25:14 +0800

    aaa

---
+++ src/Test1.java
@@ -0,0 +1,11 @@
+/**
+ * @description:
+ * @version: 1.0
+ * @author: x.x@hand-china.com
+ * @date: 2019/2/18
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("test1");
+    }
+}
